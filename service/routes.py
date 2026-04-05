d29b5709305c958cca711a540d68abd93e2bba17
rajbapat23-hub <raj.bapat23@vit.edu>
2026-04-05 01:58:09 -0400

    feat: implemented update account with 404 handling

--- service/routes.py
+++ service/routes.py
@@ -99,7 +99,25 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based on the posted data
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+
+    # 1. Find the account by ID
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+    # 2. Update the account with the new data from the request
+    account.deserialize(request.get_json())
+    account.update()
+
+    # 3. Return the updated account data
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
